a0d61141c2f9b4e6593a8b3eeff28daa4d1d0c65
TaruunMalik <taruunrmalik09@gmail.com>
2023-09-04 22:07:34 +0530

    changes

--- app/(root)/(routes)/pings/components/create-user-btn.tsx
+++ app/(root)/(routes)/pings/components/create-user-btn.tsx
@@ -1,13 +1,13 @@
-"use client";
+// "use client";
 import { Button } from "@/components/ui/button";
 import React, { useState } from "react";
 import prismadb from "@/lib/prismadb";
-import { useUser } from "@clerk/nextjs";
+import { currentUser, useUser } from "@clerk/nextjs";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 export default async function CreateUserBtn() {
   // const router = useRouter();
-  const { user } = useUser();
+  const user = await currentUser();
   const initiateProfileHandler = async () => {
     try {
       const response = await axios.post("/api/pingcreateuser/" + `${user?.id}`);
