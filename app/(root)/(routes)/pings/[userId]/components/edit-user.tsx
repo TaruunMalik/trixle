9e2102eb06286998d17f38d43fa6999f1570174d
TaruunMalik <taruunrmalik09@gmail.com>
2023-09-07 17:01:09 +0530

    edit profile

--- app/(root)/(routes)/pings/[userId]/components/edit-user.tsx
+++ app/(root)/(routes)/pings/[userId]/components/edit-user.tsx
@@ -55,7 +55,8 @@ export default function EditUser({ userId }: EditUserProps) {
           <DialogHeader>
             <DialogTitle>Edit profile</DialogTitle>
             <DialogDescription>
-              Make changes to your profile here. Click save when you're done.
+              Make changes to your profile here. Click save when you&apos;re
+              done.
             </DialogDescription>
           </DialogHeader>
           <div className="grid gap-4 py-2">
